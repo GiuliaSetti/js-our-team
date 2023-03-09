429d35a19cc6b8345c72a847b341da96bcf0603a
GiuliaSetti <giulia.setti96@gmail.com>
2023-03-09 10:04:35 +0100

    correzione : stampa in pagina

--- js/script.js
+++ js/script.js
@@ -71,46 +71,49 @@ const teamMembers = [
 // console.log (teamMembers[0].role);
 // console.log (teamMembers[0].img);
 
+const teamMembersEl = document.getElementById("our_team");
+
 for (let i = 0; i < teamMembers.length; i++) {
+
+    let newEl = document.createElement("li");
+    teamMembersEl.append(newEl);
 
     let currentMember = teamMembers[i];
 
     for(let ourTeam in currentMember){
 
-        console.log(ourTeam + ": " + currentMember[ourTeam]);
+        newEl.innerHTML += `${ourTeam}: ${currentMember[ourTeam]}<br>`;
     }
     
 }
 
 // bonus 1 e 2
 
-const teamMembersEl = document.getElementById("our_team");
 
 
-
-for (let i = 0; i < teamMembers.length; i++) {
+// for (let i = 0; i < teamMembers.length; i++) {
     
-    // elemento carta
-    const memberCard = document.createElement("div");
-    memberCard.classList.add("member_card");
-    teamMembersEl.append(memberCard);
+//     // elemento carta
+//     const memberCard = document.createElement("div");
+//     memberCard.classList.add("member_card");
+//     teamMembersEl.append(memberCard);
 
-    // immagine
-    const img = document.createElement("img");
-    img.src = "img/" + teamMembers[i].img;
-    memberCard.append(img);
+//     // immagine
+//     const img = document.createElement("img");
+//     img.src = "img/" + teamMembers[i].img;
+//     memberCard.append(img);
     
-    // nome
-    const name = document.createElement("div");
-    name.innerHTML = `${teamMembers[i].name}`;
-    name.classList.add("name");
-    memberCard.append(name);
+//     // nome
+//     const name = document.createElement("div");
+//     name.innerHTML = `${teamMembers[i].name}`;
+//     name.classList.add("name");
+//     memberCard.append(name);
     
-    // ruolo
-    const role = document.createElement("div");
-    role.innerHTML = `${teamMembers[i].role}`;
-    memberCard.append(role);
-}
+//     // ruolo
+//     const role = document.createElement("div");
+//     role.innerHTML = `${teamMembers[i].role}`;
+//     memberCard.append(role);
+// }
 
 
 
